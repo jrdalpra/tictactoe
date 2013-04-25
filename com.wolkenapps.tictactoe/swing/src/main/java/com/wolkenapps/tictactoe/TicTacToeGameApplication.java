6e7c0b94f75aec4ff7b7136aaf29d0e19ec0f2cc
José V. Dal Prá Junior <jrdalpra@gmail.com>
2013-04-25 18:21:41 -0300

    Update TicTacToeGameApplication.java

--- com.wolkenapps.tictactoe/swing/src/main/java/com/wolkenapps/tictactoe/TicTacToeGameApplication.java
+++ com.wolkenapps.tictactoe/swing/src/main/java/com/wolkenapps/tictactoe/TicTacToeGameApplication.java
@@ -305,7 +305,6 @@ public class TicTacToeGameApplication {
          for (Point point : controller.getWinnerPoints())
             findAButtonAt(point).setBackground(Color.RED);
          showMessage("The GAME has a winner");
-         aa
       } catch (TicTacToeGame.HasNoWinner noWinner) {
       } catch (TicTacToeGameApplication.ButtonNotFound notFound) {
       }
